18e44ea621dde79fd0eb6ec90e4861e9c549920e
Serg6464 <115687761+Serg6464@users.noreply.github.com>
2025-02-02 22:12:33 +0400

    опечатка

--- include/vector.h
+++ include/vector.h
@@ -9,7 +9,7 @@ public:
     vector(int x_val, int y_val);
 
     friend vector operator + (const vector &v1, const vector &v2);
-    friend int operator == (const vector &v1, const vector &v2)
+    friend int operator == (const vector &v1, const vector &v2);
 
 };
 
