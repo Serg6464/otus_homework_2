94c5d2be009ef750437adbd833a99ac6d7abc926
Serg6464 <115687761+Serg6464@users.noreply.github.com>
2025-02-02 22:02:25 +0400

    положение в пространстве

---
+++ src/vector.cpp
@@ -0,0 +1,11 @@
+#include <vector.h>
+
+vector::vector(int x_val, int y_val)
+    : x(x), y(y)
+{
+}
+
+vector operator + (const vector &v1, const vector &v2)
+{
+    return vector(v1.x + v2.x, v1.y + v2.y);
+}
---
+++ test/test_vector.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+#include <vector.h>
+
+TEST(vector, addition)
+{
+    vector pos(1, 3);
+    vector add(-2, 1);
+
+    EXPECT_EQ(pos + add, vector(-1, 2));
+}
