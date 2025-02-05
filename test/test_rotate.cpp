bc66a0c04cb4b316dc03698c68ef9dac72356292
Serg6464 <sinyakovs@inbox.ru>
2025-02-05 10:16:15 +0000

    завершены тесты rotate

--- test/test_rotate.cpp
+++ test/test_rotate.cpp
@@ -29,3 +29,33 @@ TEST(rotate, simple_turn)
 
     rotatecmd.Execute();
 }
+
+TEST(rotate, getdirection_error)
+{
+    std::shared_ptr <RotatingObjectMock> rotateobj = std::make_shared<RotatingObjectMock>();
+    Rotate rotatecmd(rotateobj);
+
+    ON_CALL( *rotateobj, get_direction()).WillByDefault(Throw("get_direction failed"));
+ 
+    EXPECT_ANY_THROW( rotatecmd.Execute() );
+}
+
+TEST(rotate, getrotation_speed_error)
+{
+    std::shared_ptr <RotatingObjectMock> rotateobj = std::make_shared<RotatingObjectMock>();
+    Rotate rotatecmd(rotateobj);
+
+    ON_CALL( *rotateobj, get_rotation_speed()).WillByDefault(Throw("get_rotation_speed failed"));
+ 
+    EXPECT_ANY_THROW( rotatecmd.Execute() );
+}
+
+TEST(rotate, set_direction_error)
+{
+    std::shared_ptr <RotatingObjectMock> rotateobj = std::make_shared<RotatingObjectMock>();
+    Rotate rotatecmd(rotateobj);
+
+    ON_CALL( *rotateobj, set_direction(_)).WillByDefault(Throw("set_direction failed"));
+ 
+    EXPECT_ANY_THROW( rotatecmd.Execute() );
+}
