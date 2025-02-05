91cea94ef194c857d9e3d7b718cfad4e5d0c66be
Serg6464 <sinyakovs@inbox.ru>
2025-02-05 09:20:08 +0000

    тесты move

--- test/test_move.cpp
+++ test/test_move.cpp
@@ -29,3 +29,36 @@ TEST(move, simple_move)
 
     movecmd.Execute();
 }
+
+TEST(move, move2_getlockation_error)
+{
+    std::shared_ptr <MovingObjectMock> moveobj = std::make_shared<MovingObjectMock>();
+    Move movecmd(moveobj);
+
+    ON_CALL( *moveobj, get_location()).WillByDefault(Throw("get_lockation failed"));
+ 
+    EXPECT_ANY_THROW( movecmd.Execute() );
+}
+
+TEST(move, move3_getvelocity_error)
+{
+    std::shared_ptr <MovingObjectMock> moveobj = std::make_shared<MovingObjectMock>();
+    Move movecmd(moveobj);
+    
+    ON_CALL( *moveobj, get_location()).WillByDefault(Return(vector(0,0)));
+    ON_CALL( *moveobj, get_velocity()).WillByDefault(Throw("get_velocityfailed"));
+ 
+    EXPECT_ANY_THROW( movecmd.Execute() );
+}
+
+TEST(move, move4_setloc_error)
+{
+    std::shared_ptr <MovingObjectMock> moveobj = std::make_shared<MovingObjectMock>();
+    Move movecmd(moveobj);
+
+    EXPECT_CALL( *moveobj, get_location()).WillOnce(Return(vector(12, 5)));
+    EXPECT_CALL( *moveobj, get_velocity()).WillOnce(Return(vector(-7, 3)));
+    ON_CALL( *moveobj, set_location ).WillByDefault(Throw("set_locationerror"));
+
+    EXPECT_ANY_THROW( movecmd.Execute() );
+}
